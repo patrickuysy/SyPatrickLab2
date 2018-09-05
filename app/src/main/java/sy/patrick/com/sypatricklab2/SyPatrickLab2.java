bd6fc7f0af967fadf55b69ed46fe14e9bbda1952
patrickuysy <2015081400@ust-ics.mygbiz.com>
2018-09-05 21:08:29 +0800

    Initial commit of Lab 2

---
+++ app/src/main/java/sy/patrick/com/sypatricklab2/SyPatrickLab2.java
@@ -0,0 +1,13 @@
+package sy.patrick.com.sypatricklab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SyPatrickLab2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+    }
+}
